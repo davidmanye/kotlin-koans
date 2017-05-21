4e344008611e733840c31b44988e0651256075d3
manye <david.manye@gmail.com>
2017-05-21 20:11:51 +0200

    refactor task10

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -20,10 +20,8 @@ fun todoTask10(): Nothing = TODO(
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
     Collections.sort(arrayList, object: Comparator<Int> {
-        override fun compare(o1: Int?, o2: Int?): Int {
-            if (o1 == null) return -1
-            if (o2 == null) return 1
-            return Comparator.reverseOrder<Int>().compare(o1, o2)
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2 - o1
         }
     })
     return arrayList
