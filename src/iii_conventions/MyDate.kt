becb3339aecedae762f2ba95a183a4b6b222552d
manye <david.manye@gmail.com>
2017-05-22 07:36:36 +0200

    Completed iii_conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,59 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return Comparator
+                .comparingInt<MyDate> { it.year }
+                .thenComparingInt { it.month }
+                .thenComparingInt { it.dayOfMonth }
+                .compare(this, other)
+    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    operator fun plus(interval: TimeInterval): MyDate {
+
+        return this.addTimeIntervals(interval, 1)
+    }
+
+    operator fun plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
+        return this.addTimeIntervals(repeatedTimeInterval.ti, repeatedTimeInterval.n)
+    }
+
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
+
+    operator fun times(num: Int): RepeatedTimeInterval {
+        return RepeatedTimeInterval(this, num)
+    }
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> {
+        return object: Iterator<MyDate> {
+
+            var current = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val ret = current
+                this.current = current.nextDay()
+                return ret
+            }
+        }
+    }
+
+    operator fun contains(d: MyDate): Boolean {
+        return start < d && d <= endInclusive
+    }
+}
